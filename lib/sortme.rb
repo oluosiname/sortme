b76db2167acb31906eca5566c13df2a00855ced5
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-06-08 16:31:35 +0100

    move files to user defined paths

--- lib/sortme.rb
+++ lib/sortme.rb
@@ -1,10 +1,38 @@
-require "filesorter/path"
+require "sortme/path"
 require "sortme/version"
 
 module Sortme
   class Sort
     def start
-      puts "dd"
+      home = File.expand_path("~")
+      Path.set unless File.file?("#{home}/sortme_settings.json")
+      count = 0
+      Dir.foreach(".") do |file|
+        if media?(file)
+          path = get_path(file)
+          count += 1 if system "mv \"#{file}\" #{path}"
+        end
+      end
+      puts "#{count} files moved"
+    end
+
+    def get_all_paths
+      Path.get
+    end
+
+    def get_path(file)
+      paths = get_all_paths
+      case File.extname(file)
+      when ".png", ".jpg", ".gif" then paths["picture"]
+      when ".flv", ".mp4", ".vob", ".avi" then paths["video"]
+      when ".mp3", ".wma", ".wav" then paths["music"]
+      end
+    end
+
+    def media?(file)
+      extensions = [".png", ".gif", ".jpg", ".flv", ".mp4", ".vob", ".avi",
+                    ".mp3", ".wma", ".wav"]
+      extensions.include? File.extname(file)
     end
   end
 
--- lib/sortme/path.rb
+++ lib/sortme/path.rb
@@ -2,7 +2,7 @@ module Sortme
   class Path
     require "json"
 
-    def set
+    def self.set
       puts "Enter video path"
       video_path = gets.chomp
 
@@ -12,12 +12,15 @@ module Sortme
       puts "Enter picture path"
       picture_path = gets.chomp
 
+      home = File.expand_path("~")
+
       system "echo '{\"video\":\"#{video_path}\",\"picture\":\"#{picture_path}\"
-      ,\"music\":\"#{music_path}\"}' > filesorter_settings.json"
+      ,\"music\":\"#{music_path}\"}' > #{@@home}/sortme_settings.json"
     end
 
-    def get
-      settings = "filesorter_settings.json"
+    def self.get
+      home = File.expand_path("~")
+      settings = "#{home}/sortme_settings.json"
       json = File.read(settings)
       JSON.parse(json)
     end

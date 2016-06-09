32f348f271e890eb936a255ff0029b9c5f8c8776
Olumuyiwa Osiname <olumuyiwa.osiname@andela.com>
2016-06-09 15:15:33 +0100

    set default path for mac

--- lib/sortme/path.rb
+++ lib/sortme/path.rb
@@ -3,6 +3,32 @@ module Sortme
     require "json"
 
     def self.set
+      puts "Do you want to use default paths? (type y if you are unsure) (y/n) "
+      choice = gets.chomp.downcase
+
+      @@mac_home = home = File.expand_path("~")
+      if choice=="y" && mac?
+        default_mac
+      elsif choice=="y" && windows?
+        default_windows
+      elsif choice=="custom"
+        custom
+      else
+        p "oops! run again"
+        exit
+      end
+
+    end
+
+    def self.mac?
+      /darwin/ =~ RUBY_PLATFORM
+    end
+
+    def self.windows?
+      /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM
+    end
+
+    def self.custom
       puts "Enter video path"
       video_path = gets.chomp
 
@@ -12,10 +38,7 @@ module Sortme
       puts "Enter picture path"
       picture_path = gets.chomp
 
-      home = File.expand_path("~")
-
-      system "echo '{\"video\":\"#{video_path}\",\"picture\":\"#{picture_path}\"
-      ,\"music\":\"#{music_path}\"}' > #{@@home}/sortme_settings.json"
+      save video_path, music_path, picture_path
     end
 
     def self.get
@@ -23,6 +46,29 @@ module Sortme
       settings = "#{home}/sortme_settings.json"
       json = File.read(settings)
       JSON.parse(json)
+    end
+
+    def self.default_mac
+      picture_path = "#{@@mac_home}/Pictures"
+      video_path = "#{@@mac_home}/Movies"
+      music_path = "#{@@mac_home}/Music"
+
+      save video_path, music_path, picture_path
+    end
+
+    def self.save(video_path, music_path, picture_path)
+      home = File.expand_path("~")
+
+      system "echo '{\"video\":\"#{video_path}\",\"picture\":\"#{picture_path}\"
+      ,\"music\":\"#{music_path}\"}' > #{home}/sortme_settings.json"
+    end
+
+    def self.default_windows
+      picture_path =
+      video_path =
+      music_path =
+
+      save video_path, music_path, picture_path
     end
   end
 end
